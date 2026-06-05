42a243fb8db9f1ff290067fe01336d14e9049c46
Guiroupe <guiiroupe@gmail.com>
2026-06-05 19:01:23 -0300

    Finalizando o exercicio de trigger

--- Lista8 - Trigger/exl8.sql
+++ Lista8 - Trigger/exl8.sql
@@ -1,4 +1,4 @@
-use DBMercado;
+USE `mercado`;
 
 #1) 
 
@@ -6,12 +6,10 @@ create table log(
 id int auto_increment primary key,
 operacao varchar(30),
 tabela varchar(30),
-data_hora time);
-
-drop trigger after_vendas; 
+data_hora datetime);
 
 DELIMITER $$
-create trigger after_vendas after insert
+create trigger after_vendas_insert after insert
 on itensvenda
 for each row
 begin
@@ -19,8 +17,6 @@ insert into log  (operacao, tabela, data_hora)
 values ('Insert', 'itensvenda', now());
 end$$
 DELIMITER ;
-
-drop trigger after_vendas_delete
 
 DELIMITER $$
 create trigger after_vendas_delete after delete
@@ -32,8 +28,6 @@ values ('Delete', 'itensvenda', now());
 end$$
 DELIMITER ;
 
-drop trigger after_vendas_update
-
 DELIMITER $$
 create trigger after_vendas_update after update
 on itensvenda
@@ -41,5 +35,35 @@ for each row
 begin
 insert into log (operacao, tabela, data_hora)
 values ('Update','itensvenda',now());
+end$$
+DELIMITER ;
+
+DELIMITER $$
+create trigger after_produtos_insert after insert
+on produtos
+for each row
+begin
+insert into log (operacao, tabela, data_hora)
+values ("Insert", "Produtos", now());
+end$$
+DELIMITER ;
+
+DELIMITER $$
+create trigger after_produtos_delete after delete
+on produtos
+for each row
+begin
+insert into log (operacao, tabela, data_hora)
+values ("Delete","Produtos",now());
+end$$
+DELIMITER ;
+
+DELIMITER $$
+create trigger after_produtos_update after update
+on produtos
+for each row
+begin
+insert into log (operacao, tabela, data_hora)
+values("Update","Produtos",now());
 end$$
 DELIMITER ;
